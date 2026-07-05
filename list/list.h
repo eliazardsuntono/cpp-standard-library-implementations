dcc3eb7900ac575c48296ad6538ee61ac90a530c
eliazardsuntono <eliazardsunton@gmail.com>
2026-07-04 22:10:34 -0400

    calling the wrong ctor, need to fix, but overall everything should be implemented, might need to create the loop within the dtor tho, idk lol

--- list/list.h
+++ list/list.h
@@ -1,14 +1,13 @@
 #include <memory>
 #include <iterator>
-#include <type_traits>
 
 namespace custom_std {
   // std::list underlying implementation uses a doubly linked list for efficient front and back insertion
   template< class T >
   struct doubly_linked_list {
-    T * data_ = nullptr;
-    T * next_ = nullptr;
-    T * prev_ = nullptr;
+    T * data_                     = nullptr;
+    doubly_linked_list<T> * next_ = nullptr;
+    doubly_linked_list<T> * prev_ = nullptr;
   };
 
   template< class T, class Allocator = std::allocator<T> >
@@ -20,8 +19,9 @@ namespace custom_std {
       using difference_type = std::ptrdiff_t;
       using reference       = value_type &;
       using const_reference = value_type const &;
-      using pointer         = std::allocator_traits<Allocator>::pointer;
+      using pointer         = typename std::allocator_traits<Allocator>::pointer;
       using const_pointer   = pointer const;
+      using double_ll_type  = doubly_linked_list<value_type>; 
       // TODO: implement custom iterator type for this class here
       // using iterator        = some implementation;
       // using const_iterator  = some implementation;
@@ -32,40 +32,48 @@ namespace custom_std {
           Allocator const & alloc = Allocator() ) : count_(count), alloc_(alloc) {
         for ( auto i{0u}; i < count_; ++i ) {
           pointer val = alloc_.allocate(sizeof(value_type));
-          if ( !head_ ) {
+          double_ll_type * cur;
+          cur->data_ = val;
+
+          if ( head_ ) {
+            cur->prev_ = tail_;
+            tail_->next_ = cur;
+            tail_ = cur;
+          } else {
             head_->data_ = val;
             tail_->data_ = val;
-          } else {
-            tail_->next = val;
-            tail_ = val;
           }
         }
       }
       template<class InputIt>
-      explicit list ( InputIt first, InputIt last, Allocator const & alloc = Allocator() ) {
-        // TODO: could be worth looking into on how to reduce code duplication for this loop here
-        for ( auto i{first}; i != last; ++i ) {
+      explicit list ( InputIt first, InputIt last, Allocator const & alloc = Allocator() ) : alloc_{alloc} {
+        for ( InputIt i{first}; i != last; ++i ) {
           pointer val = alloc_.allocate(sizeof(value_type));
-          if ( !head_ ) {
-            head_->data_ = val;
-            tail_->data_ = val;
+          double_ll_type * cur;
+          cur->data_ = *i;
+          
+          if ( head_ ) {
+            cur->prev_ = tail_;
+            tail_->next_ = cur;
+            tail_ = cur;
           } else {
-            tail_->next_ = val;
+            head_->data_ = val;
             tail_->data_ = val;
           }
           ++count_;
         }
       }
-      ~list()=default; // do I have to define dtor here? 
-      
+      ~list() {
+      }
+
       std::size_t size()      { return count_; }
       const_reference front() { return *head_->data_; }
       const_reference back()  { return *tail_->data_; }
 
     private:
       allocator_type alloc_;
-      doubly_linked_list<value_type> * head_{nullptr};
-      doubly_linked_list<value_type> * tail_{nullptr};
+      double_ll_type * head_{nullptr};
+      double_ll_type * tail_{nullptr};
       std::size_t count_;
   };
 }
