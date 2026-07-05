d292278a5515095f5c859853afc69d09687211fc
eliazardsuntono <eliazardsunton@gmail.com>
2026-07-05 14:35:09 -0400

    Edited README + made some adjustments

--- list/list.h
+++ list/list.h
@@ -5,10 +5,10 @@
 namespace custom_std {
   // std::list underlying implementation uses a doubly linked list for efficient front and back insertion
   template< class T >
-  struct doubly_linked_list {
+  struct node {
     T data_;
-    doubly_linked_list<T> * next_;
-    doubly_linked_list<T> * prev_;
+    node<T> * next_;
+    node<T> * prev_;
   };
 
   template< class T, class Allocator = std::allocator<T> >
@@ -22,7 +22,6 @@ namespace custom_std {
       using const_reference = value_type const &;
       using pointer         = typename std::allocator_traits<Allocator>::pointer;
       using const_pointer   = pointer const;
-      using double_ll_type  = doubly_linked_list<value_type>; 
       // TODO: implement custom iterator type for this class here
       // using iterator        = some implementation;
       // using const_iterator  = some implementation;
@@ -33,6 +32,8 @@ namespace custom_std {
           Allocator const & alloc = Allocator() ) 
         : alloc_{alloc}, count_{count} {
         for ( auto i{0u}; i < count_; ++i ) {
+          // TODO: clean this up alongside the iterator ctor as well...
+          // this just seems kinda messy;
           pointer val = alloc_.allocate(sizeof(value_type));
           *val = value;
 
@@ -40,7 +41,7 @@ namespace custom_std {
             head_.data_ = *val;
             tail_ = head_;
           } else {
-            double_ll_type cur;
+            node cur;
             cur.data_ = *val;
             cur.prev_ = &tail_;
             tail_.next_ = &cur;
@@ -60,7 +61,7 @@ namespace custom_std {
             head_.data_ = *val;
             tail_ = head_;
           } else {
-            double_ll_type cur;
+            node cur;
             cur.data_ = *val;
             cur.prev_ = &tail_;
             tail_.next_ = &cur;
@@ -70,7 +71,29 @@ namespace custom_std {
           ++count_;
         }
       }
-      ~list()=default;
+      ~list() { // implement this with the linkedlist 
+        node * current = &head_;
+        while ( current ) {
+          node * next = current->next_;
+          delete current;
+          current = next;
+        }
+      }
+
+      list& operator=( const_reference other ) {
+        head_  = other.head_;
+        tail_  = other.tail_;
+        count_ = other.count_;
+        return *this;
+      }
+
+      list& operator=( list && other ) noexcept {
+        if ( this != other ) {
+          // TODO: gotta finish this here
+        }
+
+        return *this;
+      }
 
       std::size_t size()      { return count_; }
       const_reference front() { return head_.data_; }
@@ -79,7 +102,7 @@ namespace custom_std {
     private:
       allocator_type alloc_;
       std::size_t count_{0};
-      double_ll_type head_;
-      double_ll_type tail_;
+      node head_;
+      node tail_;
   };
 }
