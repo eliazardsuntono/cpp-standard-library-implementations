900c29230457d3977a3019a4b641b383de07839f
eliazardsuntono <eliazardsunton@gmail.com>
2026-07-18 12:20:57 -0400

    Added all the accessors

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -4,10 +4,21 @@
 #include "vector.h"
 
 int main() {
-  custom_std::vector<int> test_list(6, 7);
-  int * data = test_list.data();
+  custom_std::vector<int> test_list{ 1, 2, 3, 4, 5 };
+  auto * data = test_list.data();
   std::size_t size = test_list.size();
-  std::cout << "first: " << data[0] << std::endl;
+  std::cout << "first: " << data[0]        << std::endl;
   std::cout << "last: "  << data[size - 1] << std::endl;
+
+  int const & cr = test_list.at(0);
+  int & ref      = test_list.at(0);
+  std::cout << "Const ref: " << cr  << std::endl;
+  std::cout << "Reference: " << ref << std::endl;
+ 
+  int const & cr_ = test_list[0];
+  int & ref_      = test_list[0];
+  std::cout << "Const ref_: " << cr  << std::endl;
+  std::cout << "Reference_: " << ref << std::endl;
+  
   return 0;
 }
--- vector/vector.h
+++ vector/vector.h
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <stdexcept>
 #include <memory>
 #include <cstddef>
 #include <iterator>
@@ -17,7 +19,7 @@ namespace custom_std {
       // using iterator               =;
       // using const_iterator         =;
       // using reverse_iterator       = std::reverse_iterator<iterator>;
-      // using const_reverse_iteraotr = std::reverse_iterator<const_iterator>
+      // using const_reverse_iterator = std::reverse_iterator<const_iterator>
 
       vector() noexcept(noexcept(Allocator())) : vector(Allocator()) {}
       explicit vector( Allocator const & alloc ) noexcept 
@@ -38,22 +40,48 @@ namespace custom_std {
           std::allocator_traits<Allocator>::construct(alloc_, data_ + i, val);
         }
       }
-      vector( vector && other );
-      vector( std::initializer_list<T> init_list, Allocator const & alloc ) 
+      vector( vector && other ) {
+        data_       = other.data_;
+        size_       = other.size_;
+        alloc_      = std::move(other.alloc_);
+        other.data_ = nullptr;
+        other.size_ = 0;
+      }
+      vector( std::initializer_list<T> init_list, Allocator const & alloc = Allocator() ) 
         : alloc_{alloc}, size_{init_list.size()} {
           data_ = std::allocator_traits<Allocator>::allocate(alloc_, size_);
           auto idx{0u}; 
-          for ( T & val : init_list ) {
+          for ( auto const & val : init_list ) {
             std::allocator_traits<Allocator>::construct(alloc_, data_ + idx, val);  
             ++idx; 
           }
         }
       ~vector() { clear(); }
+
+      reference at( size_type pos ) {
+        const_reference val = const_cast<vector const *>(this)->at(pos);
+        return const_cast<T &>(val);
+      }
+      const_reference at( size_type pos ) const {
+        if ( pos >= size_ )
+          throw std::runtime_error("Position specified > current size");
+
+        return data_[pos];
+      }
+      reference operator[]( size_type pos ) { return at(pos); }
+      const_reference operator[]( size_type pos ) const { return at(pos); }
+      reference front() { return at(0); }
+      const_reference() front const { at(0); }
+      reference back() { return at(size_ - 1); }
+      const_reference back() const { return at(size_ - 1); }
       T * const data()    const { return data_; }
       size_t const size() const { return size_; }
 
       void clear() {
-        std::allocator_traits<Allocator>::destory(alloc_, data_);
+        std::allocator_traits<Allocator>::destroy(alloc_, data_);
+      }
+      void swap() {
+
       }
 
     private:
