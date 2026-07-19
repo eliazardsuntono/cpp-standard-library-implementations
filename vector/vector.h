1010bb55dbdf66baaa2f3e0b6efea1859c62f641
eliazardsuntono <eliazardsunton@gmail.com>
2026-07-19 11:40:33 -0400

    Added a new capacity variable + finished the capacity functions

--- vector/vector.h
+++ vector/vector.h
@@ -25,37 +25,54 @@ namespace custom_std {
 
       vector() noexcept(noexcept(Allocator())) : vector(Allocator()) {}
       explicit vector( Allocator const & alloc ) noexcept 
-        : alloc_{alloc}, size_{0} {}
+        : alloc_{alloc}, size_{0}, capacity_{0} {}
       explicit vector( size_t count, T const & val, Allocator const & alloc = Allocator() )
-        : alloc_{alloc}, size_{count} {
-          data_ = std::allocator_traits<Allocator>::allocate(alloc_, count);
-          for ( auto i{0u}; i < count; ++i ) {
-            std::allocator_traits<Allocator>::construct(alloc_, data_ + i, val);
+        : alloc_{alloc}, size_{count}, capacity_{count} {
+          try {
+            data_ = std::allocator_traits<Allocator>::allocate(alloc_, count);
+            for ( auto i{0u}; i < count; ++i ) {
+              std::allocator_traits<Allocator>::construct(alloc_, data_ + i, val);
+            }
+          } catch ( std::bad_alloc const & e ) {
+            std::cout << "Error in construction: " << e.what() << std::endl;
+            throw;
           }
         }
       vector( vector const & other ) {
-        if ( size_ ) clear();
-
-        data_ = std::allocator_traits<Allocator>::allocate(alloc_, other.size_);
-        for ( auto i{0u}; i < other.size_; ++i ) {
-          auto const & val = other.data_[i];
-          std::allocator_traits<Allocator>::construct(alloc_, data_ + i, val);
+        try {
+          data_ = std::allocator_traits<Allocator>::allocate(alloc_, other.size_);
+          for ( auto i{0u}; i < other.size_; ++i ) {
+            auto const & val = other.data_[i];
+            std::allocator_traits<Allocator>::construct(alloc_, data_ + i, val);
+          }
+          size_     = other.size_;
+          capacity_ = other.capacity_;
+        } catch ( std::bad_alloc const & e ) {
+          std::cout << "Error in construction: " << e.what() << std::endl;
+          throw;
         }
       }
-      vector( vector && other ) {
+      vector( vector && other ) noexcept {
         data_       = other.data_;
         size_       = other.size_;
+        capacity_   = other.capacity_
         alloc_      = std::move(other.alloc_);
         other.data_ = nullptr;
         other.size_ = 0;
+        other.capacity_ = 0;
       }
       vector( std::initializer_list<T> init_list, Allocator const & alloc = Allocator() ) 
-        : alloc_{alloc}, size_{init_list.size()} {
-          data_ = std::allocator_traits<Allocator>::allocate(alloc_, size_);
-          auto idx{0u}; 
-          for ( auto const & val : init_list ) {
-            std::allocator_traits<Allocator>::construct(alloc_, data_ + idx, val);  
-            ++idx; 
+        : alloc_{alloc}, size_{init_list.size()}, capacity_{init_list.size()} {
+          try { 
+            data_ = std::allocator_traits<Allocator>::allocate(alloc_, size_);
+            auto idx{0u}; 
+            for ( auto const & val : init_list ) {
+              std::allocator_traits<Allocator>::construct(alloc_, data_ + idx, val);  
+              ++idx; 
+            }
+          } catch ( std::bad_alloc const & e  ) {
+            std::cout << "Error in construction: " << e.what() << std::endl;
+            throw;
           }
         }
       ~vector() { clear(); }
@@ -86,16 +103,24 @@ namespace custom_std {
       size_type size() const { return size_; }
       size_type capacity() const { return capacity_; }
       size_type max_size() const noexcept {
-        // TODO: read up on how the allocator_traits gets the max size
         return std::allocator_traits<Allocator>::max_size() / sizeof(value_type);
       }
       void reserve( size_type new_cap ) {
-        pointer dst = std::allocator_traits<Allocator>::allocate(alloc_, capacity_ + new_cap);
+        if ( new_cap <= capacity_ ) return;
+        pointer dst = std::allocator_traits<Allocator>::allocate(alloc_, new_cap);
         std::memcpy(data_, dst, size_);
         capacity_ = new_cap;
       }
       size_type capacity() const { return capacity_; }
-      void shrink_to_fit();
+      void shrink_to_fit() {
+        try{
+          if ( size_ == capacity_) return;
+          pointer dst = std::allocator_traits<Allocator>::allocate(alloc_, size_);
+          std::memcpy(dst, data_, size_);
+        } catch ( std::bad_alloc const & e ) { // NOTE: if allocation fails, then we just do nothing
+          return;
+        }
+      }
 
       void clear() {
         std::allocator_traits<Allocator>::destroy(alloc_, data_);
