91989e299f01d4ca13a703ba8c43f84e29f0327e
eliazardsuntono <eliazardsunton@gmail.com>
2026-08-17 21:09:32 -0400

    Added some more stuff

--- custom_std/include/type_traits.h
+++ custom_std/include/type_traits.h
@@ -1,23 +1,31 @@
+#include <cstddef>
+
 namespace custom_std {
 template <typename T, T v> struct integral_constant {
   using value_type = T;
   using type = integral_constant<T, v>;
-  T value = v;
+  static T const value = v;
 
   constexpr operator value_type() const noexcept { return v; }
   constexpr value_type operator()() const noexcept { return v; }
 };
 
 template <bool B> using bool_constant = integral_constant<bool, B>;
-typedef integral_constant<bool, true> true_type;
-typedef integral_constant<bool, false> false_type;
+using true_type = integral_constant<bool, true>;
+using false_type = integral_constant<bool, false>;
 
 template <class T, class U> struct is_same : false_type {
   operator bool() noexcept { return value; }
   value_type operator()() noexcept { return value; }
 };
 template <class T> struct is_same<T, T> : true_type {};
+template <class T, class U> constexpr bool is_same_v = is_same<T, U>::value;
 
 template <class T> struct is_void : is_same<T, void> {};
-template <class T> bool is_void_v = is_void<T>::value;
+template <class T> constexpr bool is_void_v = is_void<T>::value;
+
+template <class T> struct is_null_ptr : is_same<T, std::nullptr_t> {};
+template <class T> constexpr bool is_null_ptr_v = is_null_ptr<T>::value;
+
+// TODO: the rest of the stuff
 } // namespace custom_std
--- tests/type_traits_test.cpp
+++ tests/type_traits_test.cpp
@@ -7,5 +7,6 @@ int main() {
   using two_t = custom_std::integral_constant<int, 2>;
   using four_t = custom_std::integral_constant<int, 4>;
 
-  static_assert(two_t() != four_t(), "2 != 4");
+  static_assert(not custom_std::is_same_v<two_t, four_t>, "2 != 4");
+  static_assert(two_t() != four_t::value, "2 != 4");
 }
